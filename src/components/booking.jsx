2c8dc7be6ce322bb40ba50055a9ff7351d195cf1
Dinithi Goonetilleke <nawogoonetilleke@gmail.vom>
2025-03-25 19:14:39 +0530

    update 2.002

--- src/components/booking.jsx
+++ src/components/booking.jsx
@@ -2,3 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { collection, query, where, getDocs, addDoc, deleteDoc, doc, Timestamp, getDoc, orderBy, runTransaction } from 'firebase/firestore';
 import { db } from '../firebase';
 import { useAuth } from '../contexts/AuthContext';
+
+const ResourceBooking = () => {
+    const { currentUser } = useAuth();
